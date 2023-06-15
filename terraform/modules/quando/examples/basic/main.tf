0631f47767c869aa762f2486c8182205e5b20b71
Sam Holden <sholden@worth.systems>
2023-06-15 12:37:32 +0100

    basic example

---
+++ terraform/modules/quando/examples/basic/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "random_string" "random_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+module "quando" {
+  source = "../../"
+
+  name = "quando-${random_string.random_suffix.result}"
+  description   = "A lambda function that tells the time."
+  handler       = "main.handler"
+  runtime       = "python3.8"
+
+  source_path = "../../../../../src/quando"
+
+  country_code      = "US"
+}
